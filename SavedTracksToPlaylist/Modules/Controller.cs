aae0df2df12dca2718faeaeb2bdaff71f3c6a0af
Michal Stadryniak <stadryniak.michal@gmail.com>
2019-04-05 00:23:40 +0200

    Gets saved tracks ids from api

---
+++ SavedTracksToPlaylist/Modules/Controller.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SpotifyAPI.Web; //Base Namespace
+using SpotifyAPI.Web.Auth; //All Authentication-related classes
+using SpotifyAPI.Web.Enums; //Enums
+using SpotifyAPI.Web.Models; //Models for the JSON-responses
+
+namespace SavedTracksToPlaylist.Modules
+{
+    class Controller
+    {
+        public static int Select { get; set; }
+
+        public void apiController(SpotifyWebAPI _spotify)
+        {
+            switch (Select)
+            {
+                case 1:
+                    AllLibraryToPlaylist(_spotify);
+                    break;
+                default: break;
+            }
+        }
+
+        private void AllLibraryToPlaylist(SpotifyWebAPI _spotify)
+        {
+            //saves all track ids to list
+            int offset = 0;
+            int i = 1;
+            List<string> tracksIDs = new List<string>();
+            while (true)
+            {
+                Paging<SavedTrack> savedTracks = _spotify.GetSavedTracks(50, offset);
+                savedTracks.Items.ForEach(track =>
+                {
+                    tracksIDs.Add(track.Track.Id);
+                    Console.WriteLine(track.Track.Name);
+                    Console.WriteLine(i);
+                    i++;
+                });
+                offset += 50;
+                if (!savedTracks.HasNextPage())
+                {
+                    break;
+                }
+            }
+            //prints all ids of tracks
+            foreach (string id in tracksIDs)
+            {
+                    Console.WriteLine(id);
+            }
+            Console.WriteLine(offset);
+        }
+
+    }
+}
--- SavedTracksToPlaylist/Program.cs
+++ SavedTracksToPlaylist/Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using SpotifyAPI.Web; //Base Namespace
+using SpotifyAPI.Web.Auth; //All Authentication-related classes
+using SpotifyAPI.Web.Enums; //Enums
+using SpotifyAPI.Web.Models; //Models for the JSON-responses
+using SavedTracksToPlaylist.Modules;
+
 
 namespace SavedTracksToPlaylist
 {
@@ -6,7 +16,43 @@ namespace SavedTracksToPlaylist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //start menu
+            Console.WriteLine("Save Saved Tracks to Playlist");
+            Console.WriteLine("Type number of selected option and press enter");
+            Console.WriteLine("Select what do you want to do:");
+            Console.WriteLine("[1] Save all tracks from library to playlists");
+            Console.WriteLine("[0] Close");
+            try
+            {
+                Controller.Select = Int16.Parse(Console.ReadLine()); // user select to int
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("It's not a number!");
+                Console.WriteLine($"\nError message: \n{e}");
+                Console.WriteLine("\nPress any key to continue. . .");
+                Console.ReadKey();
+                System.Environment.Exit(0);
+            }
+
+            ImplicitGrantAuth auth = new ImplicitGrantAuth(Modules.AuthorizationCredits.authcode, "http://localhost:4002", "http://localhost:4002", Scope.UserLibraryRead);
+            auth.AuthReceived += AuthOnAuthReceived;
+            auth.Start();
+            auth.OpenBrowser();
+            Console.ReadKey();
+            auth.Stop(0);
+
+        }
+
+        public static void AuthOnAuthReceived(object sender, Token payload)
+        {
+            SpotifyWebAPI _spotify = new SpotifyWebAPI
+            {
+                TokenType = payload.TokenType,
+                AccessToken = payload.AccessToken
+            };
+            Controller controller = new Controller();
+            controller.apiController(_spotify);
         }
     }
 }
